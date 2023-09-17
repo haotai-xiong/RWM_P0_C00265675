7d715aad97a26d3a52337726b54c98f5c3e9a61e
haotai-xiong <c00265675@itcarlow.ie>
2023-09-17 21:03:56 +0100

    Modulo3(2nd) Test finished

--- Packages/ie.setu.cubefilter/Runtime/CubeFilter.cs
+++ Packages/ie.setu.cubefilter/Runtime/CubeFilter.cs
@@ -1,8 +1,9 @@
 public class CubeFilter
 {
+    // test CUBE
     public static int cube(int x)
     {
-        return x * x; // * x;
+        return x * x * x;
     }
 
     public static int[] cubes(int[] xs)
@@ -11,6 +12,17 @@ public class CubeFilter
         for (int i = 0; i < xs.Length; i++)
         {
             result[i] = cube(xs[i]);
+        }
+        return result;
+    }
+
+    // test MODULE3
+    public static int[] Modulo3(int[] xs)
+    {
+        int[] result = new int[xs.Length];
+        for (int i = 0; i < xs.Length; i++)
+        {
+            result[i] = xs[i] % 3;
         }
         return result;
     }
--- Packages/ie.setu.cubefilter/Tests/CubeFilterTest.cs
+++ Packages/ie.setu.cubefilter/Tests/CubeFilterTest.cs
@@ -14,5 +14,15 @@ namespace Tests
 
             CollectionAssert.AreEqual(expected, output);
         }
+
+        [Test]
+        public void CubeModulo3()
+        {
+            int[] input = { 7, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] output = CubeFilter.Modulo3(input);
+            int[] expected = { 1, 2, 2, 0, 0, 1, 1, 2, 2, 0 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
     }
 }
